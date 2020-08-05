7f3c47658475ea663eb4cdb5989c6dea37578ab8
Ulisses Mesquita <souza.mesquita@gmail.com>
2020-08-05 14:30:16 -0300

    Tela Dasboard corrigida

--- resources/views/forms_create/dashboard.blade.php
+++ resources/views/forms_create/dashboard.blade.php
@@ -1,10 +1,11 @@
+    @extends('layout.cabecalho')
 
+    @section('titulo_pagina')
+        Dashboard
+    @endsection
 
+    @section('titulo')
+        Dashboard
+    @endsection
 
-
-
-
-
-
-                           
 
