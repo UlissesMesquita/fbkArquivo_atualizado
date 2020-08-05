b63584389ee535d962c14a0f1fbe17b932a8112e
Ulisses Mesquita <souza.mesquita@gmail.com>
2020-08-05 14:29:18 -0300

    Tela Dashboard apagado

--- resources/views/forms_create/dashboard.blade.php
+++ resources/views/forms_create/dashboard.blade.php
@@ -1,84 +1,3 @@
-    @extends('layout.cabecalho')
-
-    @section('titulo_pagina')
-        Dashboard
-    @endsection
-
-    @section('titulo')
-        Dashboard
-    @endsection
-
-
-@section('conteudo')
-<div class="">
- <!-- Mostra os dados no banco de dados -->
- <div class="">
-    <table class="table table-striped">
-     <thead>
-         <tr id="Cabecalho-tabela">
- 
-             <th> Codigo</td>
-             <th>Data Principal</td>
-             <th>Emitente</td>
-             <th>Destinatária</th>    
-             <th>Tipo Documento</td>
-             <th>Número Documento</td>
-             <th>Palavra Chave</td>
-             <th>Tipo projeto</td>
-             <th>Nome Projeto</td>
-             <th>Local Arquivo</td>
-             <th>Estante</td>
-             <th>Caixa</td>
-             <th>Maço</td>
-             <th>Observações</td>
-             <th>Valor</td>
- 
-             <th>Ferramentas</th>
-         </tr>
-     </thead>
- 
-     <tbody>
- 
-         @foreach($dash as $dashboard)
-             <tr>
-                     <td scope="row">{{$dashboard->id_codigo}}</td>
-                     <td> <a href="documentos_edit/{{$dashboard->id_codigo}}" method="GET">{{date('d/m/Y', strtotime($dashboard->data))}}</a></td>                     
-                     <th>{{$dashboard->Emp_Emit}}</td>
-                     <th>{{$dashboard->Emp_Dest}}</th>    
-                     <th>{{$dashboard->tp_documento}}</td>
-                     <th>{{$dashboard->Nome_Doc}}</td>
-                     <th>{{$dashboard->Palavra_Chave}}</td>
-                     <th>{{$dashboard->Tp_Projeto}}</td>
-                     <th>{{$dashboard->nome_job}}</td>
-                     <th>{{$dashboard->Loc_Arquivo}}</td>
-                     <th>{{$dashboard->Loc_Est}}</td>
-                     <th>{{$dashboard->Loc_Box_Eti}}</td>
-                     <th>{{$dashboard->Loc_Maco}}</td>
-                     <th>{{$dashboard->Loc_Obs}}</td>
-                     <th>R${{$dashboard->Valor_Doc}}</td>
-                        
-                        <td>
-                            <!-- Botão de Editar -->
-        
-                            <a id="delete-icon" class="far fa-edit fa-2x" href="documentos_edit/{{$dashboard->id_codigo}}" method="GET" onclick=""></a>
-        
-                            <!-- Botão de Apagar -->
-        
-                            <a id="edit-icon" class="fas fa-trash fa-2x" href="delete/{{$dashboard->id_codigo}}" onclick="return confirm('Deseja realmente excluir?')" method="GET"></a>
-        
-                            <!-- Botão de Anexo -->
-                            <form method="POST" action="{{route('visualizar_anexo')}}" >
-                                @csrf 
-                                    <input type="hidden" name="id_codigo" value="{{$dashboard->id_codigo}}">
-                                    <button type="submit" class="btn btn-link" target="_blank"><i class="fa fa-file-pdf-o fa-lg" aria-hidden="true"></i></button>
-                            </form>
-        
-                        </td>
-             </tr>
-         @endforeach
-     </tbody>
- </table>
- </div>
 
 
 
@@ -86,9 +5,6 @@
 
 
 
-
-
-    @endsection
 
                            
 
