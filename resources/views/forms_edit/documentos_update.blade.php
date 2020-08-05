86ffb1c2689765332230f7fc95290df9d9fda018
Ulisses Mesquita <souza.mesquita@gmail.com>
2020-08-05 14:13:13 -0300

    Negrito na tela de update de registros

--- resources/views/forms_edit/documentos_update.blade.php
+++ resources/views/forms_edit/documentos_update.blade.php
@@ -20,17 +20,17 @@
         <!-- Linha 1 -->
         <div class="row">
                 <div class="col-md-2">
-                    <label>Código: *</label>   <a href="#"><i class="fas fa-search" id="fas fa-search"></i></a>
+                    <label><b>Código: </b></label>   <a href="#"><i class="fas fa-search" id="fas fa-search"></i></a>
                     <label for="id_codigo"></label><input type="text" class="form-control" id="id_codigo" name="id_codigo" placeholder="" value="{{$edit->id_codigo}}"  disabled >
                 </div>
 
                 <div class="col-md-2">
-                    <label>Data: *</label>
+                    <label><b>Data: </b></label>
                     <label for="data"></label><input type=text class="form-control" id="data" name="data" placeholder="" value="{{$edit->data}}" disabled>
                 </div>
 
                 <div class="col-md-4">
-                    <label>Empresa Emitente *</label>
+                    <label><b>Empresa Emitente </b></label>
                     <label for="Emp_Dest"></label>
                         <select id="Emp_Emit" name="Emp_Emit" class="form-control"  required>
                             <option selected>{{$edit->Emp_Emit}}</option>
@@ -41,7 +41,7 @@
                 </div>
     
                 <div class="col-md-4">
-                    <label>Empresa Destinatária: *</label>
+                    <label><b>Empresa Destinatária: </b></label>
                     <label for="Emp_Dest"></label>
                         <select id="Emp_Dest" name="Emp_Dest" class="form-control"  required>
                             <option selected>{{$edit->Emp_Dest}}</option>
@@ -57,7 +57,7 @@
         <div class="row">
 
             <div class="col-md-3">
-                <label>Tipo de Documento: *</label>
+                <label><b>Tipo de Documento: </b></label>
                 <label for="tp_documento"></label>
                     <select id="tp_documento" name="tp_documento" class="form-control" required onkeyup="maiuscula(this)">
                     <option selected>{{$edit->tp_documento}}</option>
@@ -69,17 +69,17 @@
             </div>
             
             <div class="col-md-3">
-                <label>Número Documento: *</label>
+                <label><b>Número Documento: </b></label>
                 <label for="Nome_Doc"></label><input type="text" class="form-control" id="Nome_Doc" name="Nome_Doc" placeholder="" value="{{$edit->Nome_Doc}}" required onkeyup="maiuscula(this)">
             </div>
 
             <div class="col-md-4">
-                <label>Assunto: *</label>
+                <label><b>Assunto: </b></label>
                 <label for="Assunto"></label><input type="text" autofocus class="form-control" id="Assunto" name="Assunto" placeholder="" value="{{$edit->Assunto}}" required onkeyup="maiuscula(this)">
             </div>
 
             <div class="col-md-2">
-                <label>Valor: *</label>
+                <label><b>Valor: </b></label>
                 <label for="Valor_Doc"></label><input type="text" class="form-control" id="Valor_Doc" name="Valor_Doc" placeholder="" value="{{$edit->Valor_Doc}}" onKeyPress="return(moeda(this,'.',',',event))">
             </div>
 
@@ -88,7 +88,7 @@
         <!-- Linha 3 -->
         <div class="row">
             <div class="col-md-3">
-                <label>Form. do Documento Arquivado: *</label>
+                <label><b>Form. do Documento Arquivado: </b></label>
                 <label for="Formato_Doc"></label>
                     <select id="Formato_Doc" name="Formato_Doc" class="form-control" required>
                         <option>{{$edit->Formato_Doc}}</option>
@@ -99,12 +99,12 @@
             </div>
 
             <div class="col-md-2">
-                <label>Data Referência: *</label>
+                <label><b>Data Referência: </b></label>
                 <label for="Dt_Ref"></label><input placeholder= "Mês/Ano" type="text"class="form-control" maxlength="7" name="Dt_Ref" value= "{{$edit->Dt_Ref}}" id="Dt_Ref" onkeyup="maiuscula(this)">
             </div>
 
             <div class="col-md-4">
-                <label>Palavra-Chave: *</label>
+                <label><b>Palavra-Chave: </b></label>
                 <label for="Palavra_Chave"></label><input type="text" class="form-control" id="Palavra_Chave" name="Palavra_Chave" placeholder="" value="{{$edit->Palavra_Chave}}" onkeyup="maiuscula(this)">
             </div>
 
@@ -114,12 +114,12 @@
         <!-- Linha 4 -->
         <div class="row">
             <div class="col-md-4">
-                <label>Descrição: *</label>
+                <label><b>Descrição: </b></label>
                 <label for="Desc"></label><input type="text" class="form-control" id="Desc" name="Desc" placeholder="" value="{{$edit->Desc}}" required onkeyup="maiuscula(this)">
             </div>
 
             <div class="col-md-4">
-                <label>Departamento: *</label>
+                <label><b>Departamento: </b></label>
                 <label for="Dep"></label>
                     <select id="Dep" name="Dep" class="form-control" required onkeyup="maiuscula(this)">
                         <option selected>{{$edit->Dep}}</option>
@@ -133,7 +133,7 @@
             </div>
 
             <div class="col-md-4">
-                <label>Origem: *</label>
+                <label><b>Origem: </b></label>
                     <label for="Origem"></label>
                         <select id="Origem" name="Origem" class="form-control" required onkeyup="maiuscula(this)">
                         <option selected>{{$edit->Origem}}</option>
@@ -150,7 +150,7 @@
          <!-- Linha 5 -->
          <div class="row">
             <div class="col-md-2">
-                <label>Tipo de Projeto: *</label>
+                <label><b>Tipo de Projeto: </b></label>
                 <label for="Tp_Projeto"></label>
                 <select id="Tp_Projeto" name="Tp_Projeto" class="form-control" onselect="mostra(Tp_Projeto)" required>
                     <option selected>{{$edit->Tp_Projeto}}</option>
@@ -160,7 +160,7 @@
             </div>
 
             <div class="col-md-10" id="drop_job" style="display: block">
-                <label>Nome do Projeto: *</label>
+                <label><b>Nome do Projeto: </b></label>
                 <label for="nome_job"></label>
                 <select id="nome_job" name="nome_job" class="form-control" >
                     <option selected>{{$edit->nome_job}}</option>
@@ -181,12 +181,12 @@
         <div class="row">
 
             <div class="col-md-2">
-                <label>Local Arquivo: </label>
+                <label><b>Local Arquivo: </b></label>
                 <label for="Loc_Arquivo"></label><input type="text" class="form-control" value="{{$edit->Loc_Arquivo}}" id="Loc_Arquivo" name="Loc_Arquivo" placeholder="" onkeyup="maiuscula(this)">
             </div>
 
             <div class="col-md-2">
-                <label>Corredor: *</label>
+                <label><b>Corredor:</b></label>
                 <label for="Loc_Cor"></label>
                 <select id="Loc_Cor" name="Loc_Cor" class="form-control" required>
                     <option selected>{{$edit->Loc_Cor}}</option>
@@ -200,7 +200,7 @@
             </div>
 
             <div class="col-md-2">
-                <label>Estante: *</label>
+                <label><b>Estante:</b></label>
                 <label for="Loc_Est"></label>
                 <select id="Loc_Est" name="Loc_Est" class="form-control" required>
                 <option selected>{{$edit->Loc_Est}}</option>
@@ -215,7 +215,7 @@
             </div>
 
             <div class="col-md-2">
-                <label>Caixa/Etiqueta: *</label>
+                <label><b>Caixa/Etiqueta:</b></label>
                 <label for="Loc_Box_Eti"></label>
                 <select id="Loc_Box_Eti" name="Loc_Box_Eti" class="form-control" required>
                 <option selected>{{$edit->Loc_Box_Eti}}</option>
@@ -230,7 +230,7 @@
             </div>
 
             <div class="col-md-2">
-                <label>Maço: *</label>
+                <label><b>Maço:</b></label>
                 <label for="Loc_Maco"></label>
                 <select id="Loc_Maco" name="Loc_Maco" class="form-control" required>
                 <option selected>{{$edit->Loc_Maco}}</option>
@@ -246,7 +246,7 @@
 
 
             <div class="col-md-2">
-                <label>Status: *</label>
+                <label><b>Status:</b></label>
                 <label for="Loc_Status"></label>
                 <select id="Loc_Status" name="Loc_Status" class="form-control" required>
                     <option>{{$edit->Loc_Status}}</option>
@@ -256,12 +256,12 @@
             </div>
 
             <div class="col-md-2">
-                <label>Desfaz/Destruir: *</label>
+                <label><b>Desfaz/Destruir: </b></label>
             <label for="Desfaz"></label><input placeholder= "" type="text" class="form-control" value= "{{$edit->Desfaz}}" name="Desfaz" maxlength="7" id="Desfaz" required onkeyup="maiuscula(this)">
             </div>
 
             <div class="col-md-6">
-                <label for="exampleFormControlTextarea1">Observações:</label>
+                <label for="exampleFormControlTextarea1"><b>Observações:</b></label>
                 <label for="Loc_Obs"></label><textarea class="form-control" id="Loc_Obs" name="Loc_Obs" rows="1" value="{{$edit->Loc_Obs}}" onkeyup="maiuscula(this)">{{$edit->Loc_Obs}}</textarea>
             </div>
 
