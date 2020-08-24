370627526b91c235505364a561f6bd06f49deda6
UlissesMesquita <souza.mesquita@gmail.com>
2020-08-24 11:22:36 -0300

    Retirado DD do controlador pesquisa

--- app/Http/Controllers/ControladorPesquisas.php
+++ app/Http/Controllers/ControladorPesquisas.php
@@ -254,7 +254,7 @@ class ControladorPesquisas extends Controller
                         $dash = empty($dados) ? Cadastro_Documentos::whereBetween('data', [$data_in, $data_out])->get(): 
                         Cadastro_Documentos::where('Dep', '=', session()->get('departamento'))->where($dados)->whereBetween('data', [$data_in, $data_out])->get();
                         $contador = $dash->count();
-                        dd($dash);
+                        //dd($dash);
                     }
 
                 }
