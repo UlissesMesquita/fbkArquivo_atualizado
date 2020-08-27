f6048badb200d58d08f7c8b30152dbe3f21b527e
Ulisses Mesquita <souza.mesquita@gmail.com>
2020-08-26 22:11:20 -0300

    Caixa resolvida

--- public/js/Javascript.js
+++ public/js/Javascript.js
@@ -1,27 +1,19 @@
 //Funções após a leitura do documento
 $(document).ready(function() {
+  
   //Select para mostrar e esconder divs
-
-
-
-
-
   $('#Dep').on('change',function(){
       var SelectValue='.'+$(this).val();
-      var SelectValueID='#'+$(this).val();
-
 
       //Desabilita o Campo
       $(".DivPai #Loc_Box_Eti").prop( "disabled", true );
       //Esconde o Campo
       $('.DivPai div').hide();
 
-      
-      // //Mostra os Campos
-      // $(SelectValue).toggle();
-      // // //Habilita o Campo
-      // $(SelectValueID).prop("disabled", false);
-
+      //Mostra os Campos
+      $(SelectValue).toggle();
+      //Habilita o Campo
+      $(SelectValue).find('select').prop("disabled", false);
 
 
   });
